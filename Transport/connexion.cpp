c93751734672fd93c5e6c97098d503f36d3e1c57
oussamabouguerra <47630354+oussamabouguerra@users.noreply.github.com>
2019-12-10 22:55:25 +0100

    Add files via upload

--- Transport/connexion.cpp
+++ Transport/connexion.cpp
@@ -1,19 +1,19 @@
 #include "connexion.h"
-#include <exception>
-Connexion::Connexion()
-{}
-
-bool Connexion::ouvrirConnexion()
+#include <QSqlError>
+connexion::connexion(){
+                      }
+bool connexion::ouvrirConnexion()
 {bool test=false;
-QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("Source_Projet2A");
-    db.setUserName("meriem");
-    db.setPassword("esprit19");
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+                           db.setDatabaseName("Source_Projet2A.");
+                           db.setUserName("ahmed");//inserer nom de l'utilisateur
+                           db.setPassword("ahmed22");//inserer mot de passe de cet utilisateur
+
 if (db.open())
     test=true;
-else
-    throw QString ("Erreur Paramétres"+test);
+
+else throw QString ("Erreur Paramétres"+db.lastError().text());
 return  test;
 }
-void Connexion::fermerConnexion()
+void connexion::fermerConnexion()
 {db.close();}
