26ec7ffd7f6f44499fbeedccb15b879c3c70e6fa
oussamabouguerra <47630354+oussamabouguerra@users.noreply.github.com>
2019-12-10 22:58:10 +0100

    Add files via upload

--- Transport/mainwindow.h
+++ Transport/mainwindow.h
@@ -1,17 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include "promtion.h"
 #include <QMainWindow>
-#include <reservation.h>
-#include <bus.h>
-#include <QMessageBox>
-#include <QDebug>
-#include <QSound>
-#include <gestiontrajet.h>
-#include <gestion.h> //anas
-#include <employe.h>
-#include <classemployee.h>
-
+#include <QtMultimedia>
+#include <QSystemTrayIcon>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -23,12 +15,46 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void refresh();
 
 private slots:
+    void on_ajouter_clicked();
 
-    void on_pushButton_login_clicked();
+
+
+    void on_supprimer_clicked();
+
+    void on_modifier_2_clicked();
+
+    void on_comboBox_3_activated(const QString &arg1);
+
+    void on_comboBox_5_activated(const QString &arg1);
+
+    void on_lineEdit_19_textChanged(const QString &arg1);
+
+    void on_checkBox_2_stateChanged(int arg1);
+
+
+    void on_pushButton_7_clicked();
+
+
+    void on_radioButton_3_clicked();
+
+    void on_radioButton_4_clicked();
+
+
+
+
+    void on_tabemployer_activated(const QModelIndex &index);
+
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    Promotion tmp;
+    int etat=0;
+    QString valeur ="";
+    QMediaPlayer *player;
+    QSystemTrayIcon *notifier;
 };
 #endif // MAINWINDOW_H
